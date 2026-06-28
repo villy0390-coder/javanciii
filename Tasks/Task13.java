d528e9d626f332f8495c4794f62c77a92b7cab4f
villy0390-coder <villy.0390@gmail.com>
2026-06-28 10:20:55 +0000

    task13

---
+++ Tasks/Task13.java
@@ -0,0 +1,44 @@
+package Tasks;
+
+class Vehicle {
+
+    String make;
+    String model;
+    int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+}
+
+class Car extends Vehicle {
+
+    int numberOfDoors;
+
+    public Car(String make, String model, int year, int numberOfDoors) {
+        super(make, model, year);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void displayDetails() {
+        System.out.println("Car Details:");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Number of Doors: " + numberOfDoors);
+    }
+
+}
+
+public class Task13 {
+ public static void main(String[] args) {
+
+        Car car = new Car("Toyota", "Camry", 2022, 4);
+
+        car.displayDetails();
+
+    }
+}
